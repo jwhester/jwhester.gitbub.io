4f4a35ee805217d14b9280777427f5ed55586efb
jwhester <jameshester88@gmail.com>
2020-01-10 23:29:06 -0600

    added social media icons to contact page

--- index.js
+++ index.js
@@ -75,37 +75,37 @@ $("#portfolio").click(function () {
         "<div class='project' id='capstone'></div>" +
         "</div>"
     );
-    $("#candyMonster").fadeIn( "slow" ).click(function () {
+    $("#candyMonster").fadeIn("slow").click(function () {
         $(this).fadeOut("slow");
-        $("#lofiWeatherMap").fadeIn( "slow" );
+        $("#lofiWeatherMap").fadeIn("slow");
     });
     $("#lofiWeatherMap").click(function () {
         $(this).fadeOut("slow");
-        $("#milHub").fadeIn( "slow" );
+        $("#milHub").fadeIn("slow");
     });
     $("#milHub").click(function () {
         $(this).fadeOut("slow");
-        $("#socialApe").fadeIn( "slow" );
+        $("#socialApe").fadeIn("slow");
     });
     $("#socialApe").click(function () {
         $(this).fadeOut("slow");
-        $("#spaceBountyHunter").fadeIn( "slow" );
+        $("#spaceBountyHunter").fadeIn("slow");
     });
     $("#spaceBountyHunter").click(function () {
         $(this).fadeOut("slow");
-        $("#designPortfolio").fadeIn( "slow" );
+        $("#designPortfolio").fadeIn("slow");
     });
     $("#designPortfolio").click(function () {
         $(this).fadeOut("slow");
-        $("#adlister").fadeIn( "slow" );
+        $("#adlister").fadeIn("slow");
     });
     $("#adlister").click(function () {
         $(this).fadeOut("slow");
-        $("#capstone").fadeIn( "slow" );
+        $("#capstone").fadeIn("slow");
     });
     $("#capstone").click(function () {
         $(this).fadeOut("slow");
-        $("#candyMonster").fadeIn( "slow" );
+        $("#candyMonster").fadeIn("slow");
     });
 
 //    carousel of images to scroll through for my portfolio at the top of the screen
@@ -132,60 +132,60 @@ $("#skills").click(function () {
         '</div>'
     );
 
-let words = document.getElementsByClassName('word');
-let wordArray = [];
-let currentWord = 0;
+    let words = document.getElementsByClassName('word');
+    let wordArray = [];
+    let currentWord = 0;
 
-words[currentWord].style.opacity = 1;
-for (let i = 0; i < words.length; i++) {
-    splitLetters(words[i]);
-}
-
-function changeWord() {
-    let cw = wordArray[currentWord];
-    let nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
-    for (let i = 0; i < cw.length; i++) {
-        animateLetterOut(cw, i);
+    words[currentWord].style.opacity = 1;
+    for (let i = 0; i < words.length; i++) {
+        splitLetters(words[i]);
     }
 
-    for (let i = 0; i < nw.length; i++) {
-        nw[i].className = 'letter behind';
-        nw[0].parentElement.style.opacity = 1;
-        animateLetterIn(nw, i);
+    function changeWord() {
+        let cw = wordArray[currentWord];
+        let nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+        for (let i = 0; i < cw.length; i++) {
+            animateLetterOut(cw, i);
+        }
+
+        for (let i = 0; i < nw.length; i++) {
+            nw[i].className = 'letter behind';
+            nw[0].parentElement.style.opacity = 1;
+            animateLetterIn(nw, i);
+        }
+
+        currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
     }
 
-    currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
-}
-
-function animateLetterOut(cw, i) {
-    setTimeout(function () {
-        cw[i].className = 'letter out';
-    }, i * 120);
-}
-
-function animateLetterIn(nw, i) {
-    setTimeout(function () {
-        nw[i].className = 'letter in';
-    }, 340 + (i * 120));
-}
-
-function splitLetters(word) {
-    let content = word.innerHTML;
-    word.innerHTML = '';
-    let letters = [];
-    for (let i = 0; i < content.length; i++) {
-        let letter = document.createElement('span');
-        letter.className = 'letter';
-        letter.innerHTML = content.charAt(i);
-        word.appendChild(letter);
-        letters.push(letter);
+    function animateLetterOut(cw, i) {
+        setTimeout(function () {
+            cw[i].className = 'letter out';
+        }, i * 120);
     }
 
-    wordArray.push(letters);
-}
+    function animateLetterIn(nw, i) {
+        setTimeout(function () {
+            nw[i].className = 'letter in';
+        }, 340 + (i * 120));
+    }
 
-changeWord();
-setInterval(changeWord, 3000);
+    function splitLetters(word) {
+        let content = word.innerHTML;
+        word.innerHTML = '';
+        let letters = [];
+        for (let i = 0; i < content.length; i++) {
+            let letter = document.createElement('span');
+            letter.className = 'letter';
+            letter.innerHTML = content.charAt(i);
+            word.appendChild(letter);
+            letters.push(letter);
+        }
+
+        wordArray.push(letters);
+    }
+
+    changeWord();
+    setInterval(changeWord, 3000);
 });
 
 //-------------------------------------------//
@@ -202,6 +202,19 @@ $("#resume").click(function () {
 //-----------------CONTACT-------------------//
 //-------------------------------------------//
 $("#contact").click(function () {
+    $('#main').html(
+        '<ul>' +
+        '<li>' +
+        '<a href="#"><i class="far fa-envelope icon"></i></a>' +
+        '</li>' +
+        '<li>' +
+        '<a href=""><i class="fab fa-linkedin-in icon"></i></a>' +
+        '</li>' +
+        '<li>' +
+        '<a href="#"><i class="fab fa-github icon"></i></a></li>' +
+        '</ul>'
+    )
+
 });
 
 
